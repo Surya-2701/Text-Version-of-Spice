f0c388e6a845e06d311f50dd5d4ff692c2cebe07
Suryapratap Singh <suryapratap2701@gmail.com>
2024-12-02 20:10:59 +0530

    Update textinput.py

--- textinput.py
+++ textinput.py
@@ -1,1 +1,11 @@
+import webbrowser
+import pyttsx3
+import requests
+
+# Initialize the text-to-speech engine
+text_to_speech = pyttsx3.init()
+
+# Replace with your actual API key and music link
+NEWS_API_KEY = "YOUR_API_KEY"
+MUSIC_LINK = "Your_music_link"
 
